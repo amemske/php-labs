c7d826e30bb3e55c523792cdc4062dad04ee6478
Antony Memia <antony@olywave.com>
2021-10-19 13:24:50 +0300

    form registration, cruds

--- registration.php
+++ registration.php
@@ -10,32 +10,56 @@
 
 //check if searchterm has been sent, if so display it
 if(isset($_POST['submit'])) {
+    $ok = true;
+
    // echo htmlspecialchars($_POST['searchterm'], ENT_QUOTES);
    //setup empty variables
 
-        if(isset($_POST['name'])){
+        if(!isset($_POST['name']) || $_POST['name'] === ''){
+            $ok = false;
+        }  else{
                 $name = $_POST['name'];
                // echo $name;
         };
-        if(isset($_POST['password'])){
+
+        if(!isset($_POST['password'])  || $_POST['password'] === ''){
+            $ok = false;
+        } else {
             $password = $_POST['password'];
         };
-        if(isset($_POST['gender'])){
+
+        if(!isset($_POST['gender']) || $_POST['gender'] === ''){
+            $ok = false;
+        } else {
             $gender = $_POST['gender'];
         };
-        if(isset($_POST['color'])){
+        
+        if(!isset($_POST['color']) || $_POST['color'] === ''){
+            $ok = false;
+        } else {
             $color = $_POST['color'];
         };
-        if(isset($_POST['languages'])){
-            $languages = $_POST['languages'];
+//languages is an array, so check if its an array, and also the length of the array
+        if(!isset($_POST['languages']) || count($_POST['languages']) === 0){
+            $ok = false;
+         } else{
+              $languages = $_POST['languages'];
         };
-        if(isset($_POST['comments'])){
+
+        if(!isset($_POST['comments']) || $_POST['comments'] === ''){
+            $ok = false;
+        } else {
             $comments = $_POST['comments'];
         };
-        if(isset($_POST['tc'])){
+
+        if(!isset($_POST['tc']) || $_POST ['tc'] === ''){
+            $ok = false;
+        } else {
             $tc = $_POST['tc'];
         };
 
+//display on the screen
+if($ok === true){
         printf('Username: %s
             <br>Password: %s
             <br>Gender: %s
@@ -51,10 +75,12 @@ if(isset($_POST['submit'])) {
             htmlspecialchars($comments, ENT_QUOTES),
             htmlspecialchars($tc, ENT_QUOTES) );
 
+           
+}
 }
  ?>
 <form action="" method="post">
-    username: <input type="text" name="searchterm"><br><br>
+    username: <input type="text" name="name"><br><br>
     Password: <input type="password" name="password"><br><br>
     Gender:
     <input type="radio" name="gender" value="f"> Female
