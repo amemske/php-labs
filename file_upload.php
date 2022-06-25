c687734605a4c83b1b918304bbe9e95c3b0fb7d5
Antony Memia <antony@olywave.com>
2022-06-25 08:49:56 +0300

    PHP revison -cookies, sesssions, files, exceptions

---
+++ file_upload.php
@@ -0,0 +1,66 @@
+<?php
+//STEPS
+//check to see if form is submitted
+//Check that the files super global is not empty
+//print_r the files super global to see what is inside
+//else add message - 'Please add a file'
+//display message in the body if it exists
+if (isset($_POST['submit'])) {
+    if (!empty($_FILES['upload']['name'])) {
+        // name is a property of the object $_FILES
+        //echo '<pre>';
+        //print_r($_FILES);
+        // echo '</pre>';
+
+        //allowed extensions
+        $allowed_ext = array('png', 'jpg', 'jpeg', 'gif');
+
+        $file_name = $_FILES['upload']['name'];
+        $file_size = $_FILES['upload']['size'];
+        $file_temp = $_FILES['upload']['tmp_name'];
+        $target_dir = "uploads/${file_name}"; //why curly brackets
+
+        // get the file extension
+        $file_ext = explode('.', $file_name);
+        $file_ext_end = (strtolower(end($file_ext)));
+        echo $file_ext_end;
+
+        //validate file extension
+        if (in_array($file_ext_end, $allowed_ext)) {
+            //validate file size
+            if ($file_size <= 8000000) {
+                //move uploaded file from temp folder to target dir
+                move_uploaded_file($file_temp, $target_dir);
+                $message = '<p style="color:green;">The file has been successfully uploaded.</p>';
+            } else {
+                $message = '<p style="color:red;">File is too large</p>';
+            }
+        } else {
+            $message = '<p style="color:red;">Invalid file type</p>';
+        }
+    } else {
+        $message = '<p style="color:red;">Please add a file</p>';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php echo $message ?? null; ?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="upload">
+        <input type="submit" value="submit" name="submit">
+    </form>
+</body>
+
+</html>
--- super_globals.php
+++ super_globals.php
@@ -1,6 +1,7 @@
 <?php
 
 /*
+superglobal are all arra
 $_GET -  Contains information  about variables passed through a URL or a form.
 $_POST - Contains information about variables passed  through a form.
 $_COOKIE - Contains information about variables passed through a cookie
@@ -10,9 +11,7 @@ $_ENV - Contains information about the environment variables
 $_FILES - Contains information about the files uploaded via HTTP POST method
 $_REQUEST - Contains information about  variables passed through the form  or URL
 
-
-
-
-
-
 */
+echo '<pre>';
+var_dump($_SERVER);
+echo '</pre>';
